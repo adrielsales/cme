129e70d24f1812a9de0a96f12cb5ce807d2b1d82
adrielsales <adrielsalles@gmail.com>
2019-02-14 15:47:38 -0300

    ajuste final na view show notícias: ajustando tamanho dos títulos e adicionando data da publicação.

--- resources/views/site/show-noticia.blade.php
+++ resources/views/site/show-noticia.blade.php
@@ -5,12 +5,10 @@
 <div class="card-painel card-panel-espacamento">
 	<div class="container">
 		<div class="row">
-		<h5>{{ $noticia->titulo }}</h5>
-		<h6>{{ $noticia->sub_titulo }}</h6>
-			<div>
-				{!! $noticia->texto !!}
-
-			</div>
+			<h4>{{ $noticia->titulo }}</h4>
+			<p style="font-style: italic; color: #666; font-size: 12px;">Publicada em {{ date('d/m/Y', strtotime($noticia->created_at)) }}</p> 
+			<h5>{{ $noticia->sub_titulo }}</h5>
+			<p>{!! $noticia->texto !!}</p>
 		</div>
 	</div>
 </div>
